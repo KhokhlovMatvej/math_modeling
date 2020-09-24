ebe9bbbd59079eec798ef83a75b9b533173bbd52
KhokhlovMatvej <71829901+KhokhlovMatvej@users.noreply.github.com>
2020-09-24 17:16:29 +0200

    Введение ........
    Было круто!!!

---
+++ untitled0.py
@@ -0,0 +1,29 @@
+type(3)
+print(type(3.4))
+print('Molodec')
+a='Molodec'
+print(a)
+print(type([1,2,3]))
+
+
+
+   
+a='Good' 
+b='Bad'
+print(a+b) #сложение строк
+a='Good' 
+b='Bad'
+
+
+
+
+
+a=[1,3,6] #создание списка
+print(a[0]) #вывод на экран элемента списка
+
+
+b=[8,10,11]
+c=a+b
+print(c)
+
+
---
+++ untitled1.py
@@ -0,0 +1,5 @@
+A=[]
+b=[]
+A.append(1)
+print(A)
+A.append(b)
