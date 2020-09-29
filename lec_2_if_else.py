b6fb63a50e85e420c0c50fb0618e03c7b95b2aff
KhokhlovMatvej <71829901+KhokhlovMatvej@users.noreply.github.com>
2020-09-29 17:46:10 +0200

    564765

---
+++ lec_2_if_else.py
@@ -0,0 +1,3 @@
+a=int(input('введите число: '))
+if a>4:
+    print('privet')
---
+++ lec_2_logic_oeration.py
@@ -0,0 +1,10 @@
+a=int(input('введите число: '))
+b=int(input('введите число: '))
+c=int(input('введите число: '))
+
+if a>4 and b==2:
+    print('Good')
+elif b>3 or c<5:
+    print('the best')
+else:
+    print('door')
