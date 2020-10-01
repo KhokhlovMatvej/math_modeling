f193aec092a09a23069b02219ea5e9f1fbcdeb76
KhokhlovMatvej <71829901+KhokhlovMatvej@users.noreply.github.com>
2020-10-01 17:43:57 +0200

    489744u43

---
+++ laba_2.py
@@ -0,0 +1,8 @@
+
+a=int(input('a: '))
+if a%2==0 and a!=0:
+    print('четное')
+elif a%2!=0 and a!=0:
+    print('нечетное')   
+else:
+    print('ни туда, ни сюда')
---
+++ lec_2_for.py
@@ -0,0 +1,17 @@
+for i in (1, 2, 3):
+    print(i**2, end=' ')
+    
+
+
+
+a=[1,6,7,8,]    
+for i in a:
+    print(i**3, end='"YES"')
+    
+ for i in range(0,10,1):
+     print(i, end=' ')
+    
+    
+    
+    
+    
