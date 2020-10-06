da9941a7124bd3e51ad74b11b35d836f86f2ea17
KhokhlovMatvej <71829901+KhokhlovMatvej@users.noreply.github.com>
2020-10-06 17:18:01 +0200

    3435465

---
+++ lab_2_5.py
@@ -0,0 +1,11 @@
+a=int(input("Введите число a: "))
+b=int(input("Введите число b: "))
+if a>b!=0:
+    print('частное', a//b) 
+    print('остаток', a%b)
+elif b==0:
+    print('не делится')
+else:
+    print(a/b)
+ 
+ 
